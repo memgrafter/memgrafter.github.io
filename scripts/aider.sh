31d8ba326979361ef5eebec6be57394ae814a1b9
Trent Robbins <robbintt@gmail.com>
2025-05-28 16:50:42 -0700

    Update aider, run script perms.

--- scripts/aider.sh
+++ scripts/aider.sh
@@ -2,11 +2,14 @@
 
 MODEL=gemini/gemini-2.5-flash-preview-05-20
 
+# editor whole was causing a lot of problems
 aider --model $MODEL \
   --dark-mode \
   --vim \
   --read ./CONVENTIONS.md \
-  --edit-format whole \
+  --file _config.yml
+  --edit-format diff \
+  --reasoning-effort high \
   --no-show-model-warnings \
   --yes-always \
   --no-git-commit-verify \
